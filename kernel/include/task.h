56f068afb26a0b0bd525d9257b5fb9c6887190bb
Piotr Miszta <piotr.miszta.pm@gmail.com>
2024-05-13 13:59:51 +0200

    Scheduler implementation
    Description:
    Scheduler implementation - RR algorythim

---
+++ kernel/include/task.h
@@ -0,0 +1,84 @@
+#ifndef TASK_H_
+#define TASK_H_
+#include "list.h"
+#include "types.h"
+
+#define list_get_task(ptr)                  \
+        list_get_ptr(ptr, struct task, list)
+
+
+/**
+ * @enum TaskStateE
+ * @brief   Status of task
+ *
+ */
+enum TaskStateE
+{
+    TASK_ACTIVE = 0,/**< TASK_ACTIVE - task is executing*/
+    TASK_READY = 1, /**< TASK_READY - task is ready to be selected by scheduler*/
+    TASK_LOCK = 2,  /**< TASK_LOCK - task is lock by mutex or semaphore */
+    TASK_END = 3,   /**< TASK_END - task is end, no longer executable */
+};
+
+/**
+ * @struct task
+ * @brief   task structure that describe all information
+ *          that are needed for execution task
+ *
+ */
+struct task
+{
+    u32 sp;                                 //stack pointer
+    u32 prio;                               //priority
+    void* params;                           //params to call function
+    i32 (*handler)(void*);                  //function to call at start of thread
+    i32 id;
+    volatile enum TaskStateE status;        //status of thread
+    struct list list;                       //list handler
+    struct list list_mtx;
+};
+
+/**
+ * @brief task_init initialize a new task
+ *
+ * @param task pointer to task structure
+ * @param handler pointer to function that should be executed by thread
+ * @param params parameters that should be passed to handler
+ * @param prio priority of task
+ * @param sp pointer to stack of task
+ * @param stack_size size of stack
+ * @return 0 on success
+ */
+i32 task_init(  struct task* task,
+                i32 (*handler)(void*),
+                void* params,
+                u32 prio,
+                u32* sp,
+                u32 stack_size);
+
+/**
+ * @fn void sched_start(u32)
+ * @brief   start scheduler, enable systick and call first task's handler
+ *          function noreturn
+ * @param ticks systick ticks
+ *
+ */
+void sched_start(u32 ticks);
+
+/**
+ * @fn struct task task_get_current*(void)
+ * @brief return pointer to struct task of currently executed task
+ *
+ * @return
+ */
+struct task* task_get_current(void);
+
+/**
+ * @fn void reschedule(void)
+ * @brief force systick
+ *
+ * @pre
+ * @post
+ */
+void reschedule(void);
+#endif /* TASK_H_ */
---
+++ kernel/src/task.c
@@ -0,0 +1,154 @@
+#include "stm32h7xx.h"
+#include "task.h"
+#include "list.h"
+
+#define SYSTICK_ENABLE      (1U << 0)
+#define SYSTICK_TICKINT     (1U << 1)
+#define SYSTICK_CLKSOURCE   (1U << 2)
+const u32 no_regs = 16;
+const u32 systick_prio = 0x0U;
+const u32 pendsv_prio = 0xffU;
+
+/* global variables */
+LIST_CREATE(task_list);
+static volatile struct task* active_task;
+static volatile struct task* next_task;
+
+/* static functions */
+static void sched_get_next_task(void);
+static i32 task_end(void* null);
+
+struct task* task_get_current(void)
+{
+    return (struct task*)active_task;
+}
+
+void reschedule(void)
+{
+    sched_get_next_task();
+    SCB->ICSR |= (1U << 28);
+}
+
+i32 task_init(  struct task* task,
+                i32 (*handler)(void*),
+                void* params,
+                u32 prio,
+                u32* sp,
+                u32 stack_size)
+{
+    u32 offset = (u32)(stack_size / sizeof(u32));
+    task->handler = handler;
+    task->params = params;
+    task->prio = prio;
+    task->sp = (u32)(sp + offset - no_regs);
+    task->status = TASK_READY;
+    task->id = list_size(&task_list);
+    /* init stack of new thread */
+    sp[offset - 1] = 0x01000000;                //psp register
+    sp[offset - 2] = (u32)handler &~ 0x01UL;    //pc register
+    sp[offset - 3] = (u32)task_end;             //lr register
+    sp[offset - 8] = (u32)params;               //r0 register - arg
+
+    /* push task to list */
+    list_add_tail(&(task->list), &task_list);
+    return 0;
+}
+
+void sched_start(u32 ticks)
+{
+    /* set systick and prio of systick pendsv*/
+    /*TODO: Move architecture depend code into new arch/ directory */
+    #ifdef CORE_CM7
+    SCB->SHPR[2] |= (u8)(pendsv_prio << 16);
+    SCB->SHPR[2] |= (u8)(systick_prio << 24);
+    #elif CORE_CM4
+    SCB->SHP[2] |= (pendsv_prio << 16);
+    SCB->SHP[2] |= (systick_prio << 24);
+    #endif
+    SysTick->LOAD = ticks - 1;
+    SysTick->CTRL |= SYSTICK_ENABLE;
+    SysTick->CTRL |= SYSTICK_TICKINT;
+    SysTick->CTRL |= SYSTICK_CLKSOURCE;
+    /* get first task to execute */
+    list_t* first_task = list_first(&task_list);
+    active_task = list_get_task(first_task);
+    /* set cpu registers */
+    u32 stack = active_task->sp + (no_regs * sizeof(u32));
+    u32 control = 0x02;
+    ASM("MSR PSP, %0" : : "r"(stack));
+    ASM("MSR control, %0" : : "r" (control) : "memory");
+    ASM("ISB 0xF" : : : "memory");
+    /* call function */
+    active_task->handler(active_task->params);
+}
+
+static void sched_get_next_task(void)
+{
+    active_task->status = TASK_READY;
+    list_t* ptr = (list_t*)(&active_task->list);
+    while(1) {
+        ptr = list_next(ptr);
+        if(list_is_head(ptr, &task_list)) {
+            continue;
+        }
+        next_task = list_get_task(ptr);
+        if(next_task->status == TASK_READY) {
+            break;
+        }
+        else if(next_task->status == TASK_LOCK) {
+            continue;
+        }
+    }
+    next_task->status = TASK_ACTIVE;
+}
+
+ATTRIBUTE(noreturn)
+static i32 task_end(void* null)
+{
+    (void)null;
+    while(1) {}
+}
+
+void SysTick_Handler(void) {
+    critical_start();
+    sched_get_next_task();
+    SysTick->VAL = 0;
+    critical_end();
+    SCB->ICSR |= (1U << 28);
+}
+
+ATTRIBUTE( isr, naked )
+void PendSV_Handler(void)
+{
+    __asm volatile(
+    "mrs    r0, psp \n"
+    "subs   r0, #16 \n"
+    "stmia  r0!,{r4-r7} \n"
+    "mov    r4, r8 \n"
+    "mov    r5, r9 \n"
+    "mov    r6, r10 \n"
+    "mov    r7, r11 \n"
+    "subs   r0, #32 \n"
+    "stmia  r0!,{r4-r7} \n"
+    "subs   r0, #16 \n"             /* Load next task's SP: */
+    "ldr    r2, =active_task \n"
+    "ldr    r1, [r2] \n"
+    "str    r0, [r1] \n"
+    "ldr    r2, =next_task \n"
+    "ldr    r1, [r2] \n"
+    "ldr    r0, [r1] \n"
+    "ldmia  r0!,{r4-r7} \n"
+    "mov    r8, r4 \n"
+    "mov    r9, r5 \n"
+    "mov    r10, r6 \n"
+    "mov    r11, r7 \n"
+    "ldmia  r0!,{r4-r7} \n"
+    "msr    psp, r0 \n"
+    "ldr r0, =0xFFFFFFFD \n"
+    "ldr    r1, =active_task \n"
+    "ldr    r2, =next_task \n"
+    "ldr    r3, [r2] \n"
+    "str    r3, [r1] \n"
+    "bx r0 \n"
+    );
+}
